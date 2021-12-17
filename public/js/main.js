7fb7609668ee414db4a10ddc0dcc27a5a4cd3f47
Todd W Crone <twcrone@gmail.com>
2021-12-16 22:53:48 -0500

    Add shadows

--- public/js/main.js
+++ public/js/main.js
@@ -1,56 +1,75 @@
 function init() {
-    let scene = new THREE.Scene();
-    let camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
-    let renderer = new THREE.WebGLRenderer();
-    let spotLight = new THREE.SpotLight(0xFFFFFF);
-    renderer.setClearColor(new THREE.Color(0x000000));
+
+    // create a scene, that will hold all our elements such as objects, cameras and lights.
+    var scene = new THREE.Scene();
+
+    // create a camera, which defines where we're looking at.
+    var camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+    // create a render and set the size
+    var renderer = new THREE.WebGLRenderer();
+
+    renderer.setClearColor(new THREE.Color(0xEEEEEE, 1.0));
     renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.shadowMapEnabled = true;
 
-    spotLight.position.set(-40, 40, -15);
-    spotLight.castShadow = true;
-    spotLight.shadowMap = new THREE.Vector2(1024, 1024);
-    spotLight.shadowCameraFar = 130;
-    spotLight.shadowCameraNear = 40;
-    scene.add(spotLight);
+    // create the ground plane
+    var planeGeometry = new THREE.PlaneGeometry(60, 20);
+    var planeMaterial = new THREE.MeshLambertMaterial({color: 0xffffff});
+    var plane = new THREE.Mesh(planeGeometry, planeMaterial);
+    plane.receiveShadow = true;
 
-    let axes = new THREE.AxisHelper(20);
-    scene.add(axes)
-
-    let planeGeometry = new THREE.PlaneGeometry(60, 20);
-    let planeMaterial = new THREE.MeshLambertMaterial({
-        color: 0xffffff
-    });
-
-    let plane = new THREE.Mesh(planeGeometry, planeMaterial);
+    // rotate and position the plane
     plane.rotation.x = -0.5 * Math.PI;
-    plane.position.set(15, 0, 0);
+    plane.position.x = 15;
+    plane.position.y = 0;
+    plane.position.z = 0;
+
+    // add the plane to the scene
     scene.add(plane);
 
     // create a cube
-    let cubeGeometry = new THREE.BoxGeometry(4, 4, 4);
-    let cubeMaterial = new THREE.MeshLambertMaterial({
-        color: 0xff0000
-    });
-    let cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
-    cube.position.set(-4, 3, 0);
+    var cubeGeometry = new THREE.BoxGeometry(4, 4, 4);
+    var cubeMaterial = new THREE.MeshLambertMaterial({color: 0xff0000});
+    var cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
+    cube.castShadow = true;
+
+    // position the cube
+    cube.position.x = -4;
+    cube.position.y = 3;
+    cube.position.z = 0;
+
+    // add the cube to the scene
     scene.add(cube);
 
-    // create a sphere
-    let sphereGeometry = new THREE.SphereGeometry(4, 20, 20);
-    let sphereMaterial = new THREE.MeshLambertMaterial({
-        color: 0x7777ff
-    });
-    let sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
-    sphere.position.set(20, 4, 2);
+    var sphereGeometry = new THREE.SphereGeometry(4, 20, 20);
+    var sphereMaterial = new THREE.MeshLambertMaterial({color: 0x7777ff});
+    var sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
+
+    // position the sphere
+    sphere.position.x = 20;
+    sphere.position.y = 4;
+    sphere.position.z = 2;
+    sphere.castShadow = true;
+
+    // add the sphere to the scene
     scene.add(sphere);
 
     // position and point the camera to the center of the scene
-    camera.position.set(-30, 40, 30);
+    camera.position.x = -30;
+    camera.position.y = 40;
+    camera.position.z = 30;
     camera.lookAt(scene.position);
 
-    // add the output of the renderer to the html element
-    document.getElementById("webgl-output").appendChild(renderer.domElement);
+    // add spotlight for the shadows
+    var spotLight = new THREE.SpotLight(0xffffff);
+    spotLight.position.set(-40, 60, -10);
+    spotLight.castShadow = true;
+    scene.add(spotLight);
 
-    // render the scene
+    // add the output of the renderer to the html element
+    document.getElementById("WebGL-output").appendChild(renderer.domElement);
+
+    // call the render function
     renderer.render(scene, camera);
 }
