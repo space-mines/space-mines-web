6cf668dbeeb278a048ba451bfbbfd9bd34d7d6d5
Todd W Crone <twcrone@gmail.com>
2021-12-16 22:22:45 -0500

    Add spotlight and new materials

--- public/js/main.js
+++ public/js/main.js
@@ -2,15 +2,23 @@ function init() {
     let scene = new THREE.Scene();
     let camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
     let renderer = new THREE.WebGLRenderer();
+    let spotLight = new THREE.SpotLight(0xFFFFFF);
     renderer.setClearColor(new THREE.Color(0x000000));
     renderer.setSize(window.innerWidth, window.innerHeight);
+
+    spotLight.position.set(-40, 40, -15);
+    spotLight.castShadow = true;
+    spotLight.shadowMap = new THREE.Vector2(1024, 1024);
+    spotLight.shadowCameraFar = 130;
+    spotLight.shadowCameraNear = 40;
+    scene.add(spotLight);
 
     let axes = new THREE.AxisHelper(20);
     scene.add(axes)
 
     let planeGeometry = new THREE.PlaneGeometry(60, 20);
-    let planeMaterial = new THREE.MeshBasicMaterial({
-        color: 0xAAAAAA
+    let planeMaterial = new THREE.MeshLambertMaterial({
+        color: 0xffffff
     });
 
     let plane = new THREE.Mesh(planeGeometry, planeMaterial);
@@ -20,9 +28,8 @@ function init() {
 
     // create a cube
     let cubeGeometry = new THREE.BoxGeometry(4, 4, 4);
-    let cubeMaterial = new THREE.MeshBasicMaterial({
-        color: 0xFF0000,
-        wireframe: true
+    let cubeMaterial = new THREE.MeshLambertMaterial({
+        color: 0xff0000
     });
     let cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
     cube.position.set(-4, 3, 0);
@@ -30,9 +37,8 @@ function init() {
 
     // create a sphere
     let sphereGeometry = new THREE.SphereGeometry(4, 20, 20);
-    let sphereMaterial = new THREE.MeshBasicMaterial({
-        color: 0x7777FF,
-        wireframe: true
+    let sphereMaterial = new THREE.MeshLambertMaterial({
+        color: 0x7777ff
     });
     let sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
     sphere.position.set(20, 4, 2);
